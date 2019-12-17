0f9c340a620ee0f093996413e7d3bf28b1e388ce
Zohaib Sibte Hassan <zohaib.hassan@doordash.com>
2019-12-17 08:06:47 -0800

    Template updates

--- src/main/kotlin/ServiceModule.kt
+++ src/main/kotlin/ServiceModule.kt
@@ -2,7 +2,6 @@ import com.doordash.common.monitoring.CommonMeterRegistry
 import com.doordash.common.monitoring.MeterRegistryModule
 import com.doordash.common.monitoring.SentryClient
 import com.doordash.common.request.InstrumentationInterceptor
-import com.doordash.common.request.MeterInterceptor
 import com.doordash.common.request.SentryReporterInterceptor
 import com.doordash.logging.KontextLogger
 import com.google.inject.Inject
@@ -10,8 +9,6 @@ import com.google.inject.Provides
 import com.google.inject.Singleton
 import dev.misfitlabs.kotlinguice4.KotlinModule
 import io.grpc.examples.helloworld.GreeterCoroutineGrpc
-import io.micrometer.core.instrument.composite.CompositeMeterRegistry
-import io.micrometer.statsd.StatsdConfig
 
 class ServiceModule : KotlinModule() {
     override fun configure() {
@@ -27,14 +24,6 @@ class ServiceModule : KotlinModule() {
     @Inject
     @Singleton
     fun instrumentationReporter(meterRegistry: CommonMeterRegistry.DefaultMeterRegistry) = InstrumentationInterceptor(
-        meterRegistry,
-        KontextLogger.logger("Instrumentation")
-    )
-
-    @Provides
-    @Inject
-    @Singleton
-    fun meterReporter(meterRegistry: CommonMeterRegistry.DefaultMeterRegistry) = MeterInterceptor(
         meterRegistry,
         KontextLogger.logger("Instrumentation")
     )
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,7 @@
 import com.doordash.common.request.InstrumentationInterceptor
-import com.doordash.common.request.MeterInterceptor
 import com.doordash.common.request.SentryReporterInterceptor
 import com.doordash.common.service.ServiceConfig
 import com.doordash.logging.KontextLogger
-import com.github.marcoferrer.krotoplus.coroutines.withCoroutineContext
 import com.google.inject.Guice
 import dev.misfitlabs.kotlinguice4.getInstance
 import io.grpc.Channel
@@ -14,8 +12,6 @@ import io.grpc.StatusRuntimeException
 import io.grpc.examples.helloworld.GreeterCoroutineGrpc
 import io.grpc.examples.helloworld.GreeterGrpc
 import io.grpc.examples.helloworld.newHelloRequest
-import io.grpc.inprocess.InProcessChannelBuilder
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.guava.await
 import kotlinx.coroutines.runBlocking
@@ -29,9 +25,8 @@ fun main() {
 
     val server: Server = ServerBuilder
         .forPort(50051)
-        // .intercept(injector.getInstance<SentryReporterInterceptor>())
-        // .intercept(injector.getInstance<InstrumentationInterceptor>())
-        .intercept(injector.getInstance<MeterInterceptor>())
+        .intercept(injector.getInstance<SentryReporterInterceptor>())
+        .intercept(injector.getInstance<InstrumentationInterceptor>())
         .addService(
             injector.getInstance<GreeterCoroutineGrpc.GreeterImplBase>()
         )
@@ -43,6 +38,7 @@ fun main() {
         callClient()
     }
 
+    logger.info("Bye cruel world....")
     server.shutdown()
     server.awaitTermination()
 }
@@ -54,8 +50,8 @@ suspend fun callClient() {
         .build()
 
     // Optional coroutineContext. Default is Dispatchers.Unconfined
-    val stub = GreeterGrpc
-        .newFutureStub(channel)
+    val stub = GreeterCoroutineGrpc
+        .newStubWithContext(channel)
 
     repeat(10) {
         try {
@@ -69,11 +65,11 @@ suspend fun callClient() {
     delay(1000)
 }
 
-suspend fun performUnaryCall(i: Int, stub: GreeterGrpc.GreeterFutureStub) {
+suspend fun performUnaryCall(i: Int, stub: GreeterCoroutineGrpc.GreeterCoroutineStub) {
     val name = if (i % 3 == 0) "deadp00l" else "dead"
     val unaryResponse = stub.sayHello(newHelloRequest {
         this.name = name
-    }).await()
+    })
 
     logger.info("Unary Response: ${unaryResponse.message}")
 }
